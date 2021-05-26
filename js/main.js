1fc14dd293c6ab2104d693aa007a622a93a03736
kozimov786 <email@youremail.com>
2021-05-26 15:22:12 +0500

    add responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 window.addEventListener('DOMContentLoaded',()=>{
-    let openBurger = document.querySelector('.burger-btn')
+    let openBurger = document.querySelector('.burger')
     let closeBurger = document.querySelector('.active-burger')
 
     openBurger.addEventListener('click',()=>{
         document.querySelector('.sitenav').classList.toggle('active-burger')
         document.querySelector('.baskets-wrapper').classList.toggle('active-burger')
     })
+    const burger = document.querySelector(".burger");
+
+    burger.addEventListener("click", function () {
+    const body = document.body;
+    body.classList.toggle("nav-open");
+});
     
 })
