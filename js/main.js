8d14d7c7e3912c0ee401aceba49ec1e93221ecfc
kozimov786 <email@youremail.com>
2021-05-27 05:48:08 +0500

    add some responsive styles

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ window.addEventListener('DOMContentLoaded',()=>{
     openBurger.addEventListener('click',()=>{
         document.querySelector('.sitenav').classList.toggle('active-burger')
         document.querySelector('.baskets-wrapper').classList.toggle('active-burger')
+        document.querySelector('.search-btn').classList.toggle('disabled-search')
+    })
+    document.querySelector('.search-btn')('click',()=>{
+        document.querySelector('.input-search').classList.toggle('search-enabled')
     })
     const burger = document.querySelector(".burger");
 
