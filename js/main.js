f2f2dba5efa29f74cc6efca77510cda0646313e6
kozimov786 <email@youremail.com>
2021-06-03 14:05:10 +0500

    add sidebar

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded',()=>{
         document.querySelector('.baskets-wrapper').classList.toggle('active-burger')
         document.querySelector('.search-btn').classList.toggle('disabled-search')
     })
-    document.querySelector('.search-btn')('click',()=>{
+    document.querySelector('.search-btn').addEventListener('click',()=>{
         document.querySelector('.input-search').classList.toggle('search-enabled')
     })
     const burger = document.querySelector(".burger");
@@ -20,9 +20,35 @@ window.addEventListener('DOMContentLoaded',()=>{
 
 
 });
-var slider = tns({
-    container: '.slides',
-    items: 1,
-    slideBy: 'page',
-    autoplay: true
+
+
+
+
+  let min = 1;
+  let max = 100;
+  
+  const calcLeftPosition = (value) => (100 / (100 - 10)) * (value - 10);
+  
+  $("#rangeMin").on("input", function (e) {
+    const newValue = parseInt(e.target.value);
+    if (newValue > max) return;
+    min = newValue;
+    $("#thumbMin").css("left", calcLeftPosition(newValue) + "%");
+    $("#min").html(newValue);
+    $("#line").css({
+      left: calcLeftPosition(newValue) + "%",
+      right: 100 - calcLeftPosition(max) + "%"
+    });
+  });
+  
+  $("#rangeMax").on("input", function (e) {
+    const newValue = parseInt(e.target.value);
+    if (newValue < min) return;
+    max = newValue;
+    $("#thumbMax").css("left", calcLeftPosition(newValue) + "%");
+    $("#max").html(newValue);
+    $("#line").css({
+      left: calcLeftPosition(min) + "%",
+      right: 100 - calcLeftPosition(newValue) + "%"
+    });
   });
